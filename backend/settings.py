ef3a5093343479966f43e4b63e7bcbfb77541f07
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2022-03-24 21:09:13 -0600

    debug symbol changed to false

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "e^b34l68jzk@(&+y41ftkqfb$vxh0rua#@k8h-sd%=-g^c66od"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
