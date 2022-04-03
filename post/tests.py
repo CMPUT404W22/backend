d1a690df2878a3512acdcdad983a174a8ffa0987
linrui06 <linrui@ualberta.ca>
2022-04-03 14:36:04 -0600

    updated of test of post

--- post/tests.py
+++ post/tests.py
@@ -1,4 +1,5 @@
-from post.models import Post
+from unicodedata import category
+from post.models import Post, Visibility
 from author.models import Author
 from rest_framework.test import APITestCase
 from rest_framework.test import APIClient
@@ -6,7 +7,7 @@ from rest_framework import status
 
 
 
-class FollowersTestCase(APITestCase):
+class PostTestCase(APITestCase):
 
     def setUp(self):
         # create users
@@ -23,8 +24,11 @@ class FollowersTestCase(APITestCase):
         self.author1 = Author.objects.get(username="user1")
         self.foreignId1 = self.author1.id
         
+        self.author2 = Author.objects.get(username="user1")
+        self.foreignId2 = self.author2.id
+        
         # create a post 
-        self.post = Post.objects.create(
+        self.post1 = Post.objects.create(
             id=self.id, 
             author = self.user,
             type = 0,
@@ -35,10 +39,24 @@ class FollowersTestCase(APITestCase):
             unlisted = False,
             categories = "Nice"
         )
-        # image post
+        
+        self.post2 = Post.objects.create(
+            id = self.foreignId1,
+            author = self.author1,
+            type = 0,
+            title = "Title",
+            description = "Description",
+            content = "Content",
+            visibility = 1,
+            unlisted = False,
+            categories = "Cate"
+        )
+        
+        # set a image post
+        self.post_image_id ="2566778"
         self.Image_Post = {
-            "id" : self.id,
-            "author" : self.id,
+            "id" : self.post_image_id,
+            "author" : self.user,
             "type" : 3,
             "title" : "ImgTitle",
             "description" : "ImgDescription",
@@ -52,25 +70,30 @@ class FollowersTestCase(APITestCase):
         # Authenticate user
         self.client = APIClient()
         self.client.login(username='test1', password='password')
+        self.client.login(username='user1', passowrd='password')
 
-    def test_get_post(self):
+    def test_get_posts(self):
         # test getting posts
-        response = self.client.get(f'/service/authors/{self.id}/posts/{self.id}')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(self.post.id, self.id)
-        self.assertEqual(self.post.author, self.user)
-        self.assertEqual(self.post.type, 0)
-        self.assertEqual(self.post.title, "TheTitle")
-        self.assertEqual(self.post.description, "TheDescription")
-        self.assertEqual(self.post.content, "Nice Day!")
-        self.assertEqual(self.post.visibility, 0)
-        self.assertEqual(self.post.unlisted, False)
-        self.assertEqual(self.post.categories, "Nice")
+        response1 = self.client.get(f'/service/authors/{self.id}/posts/{self.id}')
+        response2 = self.client.get(f'/service/authors/{self.foreignId1}/posts/{self.foreignId1}')
+        self.assertEqual(response1.status_code, status.HTTP_200_OK)
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
+        # if self.post1 works, then self.post2 works in the same way
+        self.assertEqual(self.post1.id, self.id)
+        self.assertEqual(self.post1.author, self.user)
+        self.assertEqual(self.post1.type, 0)
+        self.assertEqual(self.post1.title, "TheTitle")
+        self.assertEqual(self.post1.description, "TheDescription")
+        self.assertEqual(self.post1.content, "Nice Day!")
+        self.assertEqual(self.post1.visibility, 0)
+        self.assertEqual(self.post1.unlisted, False)
+        self.assertEqual(self.post1.categories, "Nice")
 
     def test_post_post(self):
         request = self.Image_Post
         response = self.client.post(f'/service/authors/{self.id}/posts/', request)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
 
     def test_delete_posts(self):
         # test deleting a post
@@ -78,9 +101,17 @@ class FollowersTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
         self.assertEqual(response.content, b'"Deleted"')
         
+    def test_invalid_post_post(self):
+        # test when tring to post a post to a invalid author
+        request = self.Image_Post
+        invalidId = "45673952"
+        response = self.client.post(f'/service/authors/{invalidId}/posts/', request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
     def test_invalid_delete(self):
         # test when trying to delete follower that isnt in database
-        response = self.client.delete(f'/service/authors/{self.id}/posts/{self.foreignId1}')
+        invalidId = "45673952"
+        response = self.client.delete(f'/service/authors/{self.id}/posts/{invalidId}')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_invalid_get(self):
@@ -89,6 +120,3 @@ class FollowersTestCase(APITestCase):
         response = self.client.delete(f'/service/authors/{self.id}/posts/{invalidId}')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         
-    def test_add_post_invalid_request(self):
-        response = self.client.post(f'/service/authors/{self.foreignId1}/posts/', {})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
