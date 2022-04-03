2b2172bba8eae5a7c5ca27f418385c8a56d9d3ae
Anita-00 <ferenc@ualberta.ca>
2022-04-03 17:35:44 -0600

    changed host

--- author/host.py
+++ author/host.py
@@ -1,2 +1,2 @@
 from backend.settings import DEBUG
-base_url = "http://127.0.0.1:8000" if DEBUG else "https://cmput404-w22-project-backend.herokuapp.com"
+base_url = "http://127.0.0.1:8000" if False else "https://cmput404-w22-project-backend.herokuapp.com"
